c419b065ee1f3ab65fcac0a506b42d2db721400c
xingyi.hu <xingyi.hu@shopee.com>
2023-01-11 20:55:59 +0800

    feat: add auth and jwt

---
+++ api/src/auth/auth.service.ts
@@ -0,0 +1,71 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { hash, compare } from 'bcrypt';
+import { UserService } from '../user/user.service';
+import { NewUserDTO } from '../user/dtos/new-user.dto';
+import { UserDetails } from '../user/user-details.interface';
+import { ExistingUserDTO } from '../user/dtos/existing-user.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  async hashPassword(password: string): Promise<string> {
+    return hash(password, 12);
+  }
+
+  async register(user: Readonly<NewUserDTO>): Promise<UserDetails | any> {
+    const { name, email, password } = user;
+
+    const existingUser = await this.userService.findByEmail(email);
+
+    if (existingUser) return 'Email taken!';
+
+    const hashedPassword = await this.hashPassword(password);
+
+    const newUser = await this.userService.create(name, email, hashedPassword);
+    return this.userService._getUserDetails(newUser);
+  }
+
+  async doesPasswordMatch(
+    password: string,
+    hashedPassword: string,
+  ): Promise<boolean> {
+    return compare(password, hashedPassword);
+  }
+
+  async validateUser(
+    email: string,
+    password: string,
+  ): Promise<UserDetails | null> {
+    const user = await this.userService.findByEmail(email);
+    const doesUserExist = !!user;
+
+    if (!doesUserExist) return null;
+
+    const doesPasswordMatch = await this.doesPasswordMatch(
+      password,
+      user.password,
+    );
+
+    if (!doesPasswordMatch) return null;
+
+    return this.userService._getUserDetails(user);
+  }
+
+  async login(
+    existingUser: ExistingUserDTO,
+  ): Promise<{ token: string } | null> {
+    const { email, password } = existingUser;
+    const user = await this.validateUser(email, password);
+
+    if (!user) return null;
+
+    const jwt = await this.jwtService.signAsync({ user });
+
+    return { token: jwt };
+  }
+}

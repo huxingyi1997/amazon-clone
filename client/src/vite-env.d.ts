be75edbbd3491e5d3c6bc225e44832dfc75a285a
xingyi.hu <xingyi.hu@shopee.com>
2023-12-29 15:59:32 +0800

    feat: add compodoc

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_REACT_APP_BASE_API: string;
+  readonly VITE_STRIPE_SECRET_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
